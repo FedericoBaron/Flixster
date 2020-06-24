2bb8e778140357fb29fa04c7fad050499e01eac9
Federico Agustin Barón <federicobaron@fb.com>
2020-06-24 16:48:23 -0400

    Created Movies class

--- app/src/main/java/com/example/flixster/MainActivity.java
+++ app/src/main/java/com/example/flixster/MainActivity.java
@@ -7,18 +7,24 @@ import android.util.Log;
 
 import com.codepath.asynchttpclient.AsyncHttpClient;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.example.flixster.models.Movie;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.List;
+
 import okhttp3.Headers;
 
 public class MainActivity extends AppCompatActivity {
-    //PRODUCTION
+    // PRODUCTION
     public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=4d369090c5ea76c8dacd3d7d9c78a468";
-    //SAMPLE
+    // SAMPLE
     //public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
     public static final String TAG = "MainActivity";
+
+    // List of movie objects
+    List<Movie> movies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         AsyncHttpClient client = new AsyncHttpClient();
+
+        //GET API call
         client.get(NOW_PLAYING_URL, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Headers headers, JSON json) {
@@ -34,6 +42,11 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     JSONArray results = jsonObject.getJSONArray("results");
                     Log.i(TAG, "Results: " + results.toString());
+
+                    movies = Movie.fromJsonArray(results);
+
+                    Log.i(TAG, "Movies: " + movies.size());
+
                 } catch(Exception e) {
                     Log.e(TAG, "Hit JSON exception", e);
                 }
